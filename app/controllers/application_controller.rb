f28f3da80d996cd026ae0cfe58823d6ddfce7446
深山　俊 <macnel@fukayamashunnoMacBook-Pro.local>
2020-07-19 22:20:47 +0900

    Add

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+	before_action :authenticate_user!,except: [:top] #ログインしていなければログイン画面へリダイレクト
+
+	before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+ def top
+ end
+
+end
